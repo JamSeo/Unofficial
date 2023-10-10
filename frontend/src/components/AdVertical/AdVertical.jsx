4f14269536eda378dda85d2c55c5a08b415364f3
Sseo Jjam <sjh960815@gmail.com>
2023-10-10 23:47:28 +0900

    Initial commit

---
+++ frontend/src/components/AdVertical/AdVertical.jsx
@@ -0,0 +1,20 @@
+import styles from './AdVertical.module.css'
+import { useEffect, useState } from "react";
+export default function AdVertical(){
+
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+  return(
+    <>
+      <div className={styles.advertiseVer}>
+        <ins className="adsbygoogle"
+        style={{display:'block'}}
+        data-ad-client="ca-pub-2110899329559840"
+        data-ad-slot="4214547025"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+      </div>
+    </>
+  );
+}
